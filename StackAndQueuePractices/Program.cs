fab205995fab3a37ce66ce37af4fc554cebe2b00
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-25 16:56:59 +0530

    Adding welcome message

---
+++ StackAndQueuePractices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueuePractices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the DataStructures Stack and Queue Practices");
+        }
+    }
+}
