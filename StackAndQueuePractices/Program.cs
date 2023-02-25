4576bee02603a80b0ae9ca503f036943377b3e21
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-25 17:13:46 +0530

    Created a stack using push method UC1_CreateAStack

--- StackAndQueuePractices/Program.cs
+++ StackAndQueuePractices/Program.cs
@@ -7,6 +7,19 @@ namespace StackAndQueuePractices
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the DataStructures Stack and Queue Practices");
+            Console.WriteLine("1.Create a stack");
+            Console.WriteLine("Choose above option");
+            int option = Convert.ToInt32(Console.ReadLine());  
+            switch (option)
+            {
+                case 1:
+                    StackOperation operation = new StackOperation();
+                    operation.push(70);
+                    operation.push(30);
+                    operation.push(56);
+                    operation.Display();
+                    break;
+            }
         }
     }
 }
---
+++ StackAndQueuePractices/StackOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueuePractices
+{
+    public class StackOperation
+    {
+        private Node top;
+        public StackOperation()
+        {
+            this.top = null;
+        }
+        public void push (int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed into the stack",value);
+        }
+    }
+}
